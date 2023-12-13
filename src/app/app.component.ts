162672f25e0cd882f6c2d1aa4b662fad8617c0c2
adithk <adithk1308@gmail.com>
2023-12-13 17:14:31 +0530

    project first stage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,49 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo';
+
+  title = 'TODO APP';
+  todos:Todo[]=[];
+
+  ngOnInit(){
+    this.todos=JSON.parse(localStorage.getItem('todos') as string);
+
+
+  }
+
+addTodo(form:NgForm) {
+  let formData=form.value;
+
+  this.todos.push({
+    id:this.getRandom(10),
+    heading: formData.todoHeading,
+    description: formData.todoDescription,
+    createdTime: new Date(),
+    isCompleted: false
+  });
+  localStorage.setItem('todos',JSON.stringify(this.todos));
+}
+
+removeTodo(todo: Todo) {
+  this.todos.splice(this.todos.indexOf(todo),1);
+  localStorage.setItem('todos',JSON.stringify(this.todos));
+
+}
+
+toggleStatus(todo:Todo){
+  todo.isCompleted=!todo.isCompleted;
+}
+
+getRandom(length:number){
+    return Math.floor( Math.random()*Math.pow(10,length));
+}
+
+}
+
+export interface Todo{
+  id:number,
+  heading:'string',
+  description:string,
+  createdTime:Date,
+  isCompleted:boolean;
 }
